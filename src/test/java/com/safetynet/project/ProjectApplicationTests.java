bb546d6d6055427bd0349970993c44ab8da75530
libra1919 <aymentli@gmail.com>
2021-03-25 19:56:14 +0100

    Initial commit

---
+++ src/test/java/com/safetynet/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
